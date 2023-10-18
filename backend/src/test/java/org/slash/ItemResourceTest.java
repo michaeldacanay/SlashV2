372eb667b7ae8382ed04b461fd96a05dcf544a4a
Ingmar Fjolla <ingmar.fjolla09@myhunter.cuny.edu>
2023-10-18 15:18:32 -0400

    added some quarkus tests

--- backend/src/test/java/org/slash/ItemResourceTest.java
+++ backend/src/test/java/org/slash/ItemResourceTest.java
@@ -1,11 +1,13 @@
 package org.slash;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.slash.models.Item;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
-
+import static org.assertj.core.api.Assertions.assertThat;
 @QuarkusTest
 public class ItemResourceTest {
 
@@ -18,4 +20,43 @@ public class ItemResourceTest {
              .body(is("Hello from RESTEasy Reactive"));
     }
 
+    @Test
+    public void testGetAllItems(){
+        given()
+                .when().get("/api/all")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON);
+
+    }
+
+    @Test
+    public void testGetByItem(){
+        given()
+                .when().get("/api/item/laptops")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON);
+
+    }
+
+    @Test
+    public void testGetByStore(){
+        given()
+                .when().get("/api/amazon")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON);
+
+    }
+
+    @Test
+    public void testGetByItemAndStore(){
+        given()
+                .when().get("/api/laptop/amazon")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON);
+
+    }
 }
