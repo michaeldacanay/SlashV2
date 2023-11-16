72651782b3595b8e0f334f467b8db3425b9fd839
xliang8 <xliang8@ncsu.edu>
2023-11-16 00:07:31 -0500

    add current changes

--- frontend/src/components/DataDisplay.js
+++ frontend/src/components/DataDisplay.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataTable } from 'primereact/datatable/datatable.esm.js';
 import { Column } from 'primereact/column/column.esm.js';
 import 'primereact/resources/themes/saga-purple/theme.css'
@@ -6,6 +6,7 @@ import 'primereact/resources/primereact.min.css'
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import RequestModal from "./RequestModal.js";
+import fx from 'money';
 
 function DataDisplay() {
     const location = useLocation();
@@ -14,6 +15,56 @@ function DataDisplay() {
     const isModalOpen = location.state ? location.state.isModalOpen : null;
     const navigate = useNavigate();
 
+    const [selectedCurrency, setSelectedCurrency] = useState('EUR');
+    const handleCurrencyChange = (newCurrency) => {
+        setSelectedCurrency(newCurrency);
+    };
+
+    const convertToDifferentCurrency = (price) => {
+        // USD($)', 'EUR(€)', 'JPY(¥)', 'INR(₹)', 'GBP(£)', 'AUD($)', 'CAD($)
+        fx.base = "USD"; // Adjust this based on your data
+        fx.rates = {
+            "USD": 1,
+            "EUR": 0.85,
+            "GBP": 0.75,
+            // Add more currencies as needed
+        };
+        // Check if both source and target currencies are defined in rates
+        if (!fx.rates[fx.base]) {
+            throw new Error("Invalid form currency codes");
+        }
+        if (!fx.rates["EUR"]) {
+            throw new Error("Invalid to currency codes");
+        }
+        // console.log(typeof selectedCurrency);
+        // console.log(typeof fx.rates.EUR);
+        // console.log('Price before conversion:', price);
+        // return fx(price).to(selectedCurrency);
+        return fx(price).from("USD").to(selectedCurrency).toFixed(2);
+    };
+
+    const currencyDropdown = (
+        <select value={selectedCurrency} onChange={(e) => handleCurrencyChange(e.target.value)}>
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="GBP">GBP</option>
+            {/* Add more currencies as needed */}
+        </select>
+    );
+
+
+    const priceBodyTemplate = (rowData) => {
+        const price_in_us = rowData.price;
+
+        const priceAsInt = parseInt(price_in_us);
+        const convertedPrice = convertToDifferentCurrency(priceAsInt);
+
+        // console.log(priceAsInt)
+        // const priceAsFloat = parseFloat(price_in_us);
+        // const convertedPrice = convertToDifferentCurrency(priceAsFloat);
+        return convertedPrice;
+    };
+
     const HandleSubmission = async () => {
         try {
             navigate('/search')
@@ -21,10 +72,6 @@ function DataDisplay() {
             console.log(error);
         }
     }
-
-
-
-
 
     if (data === null || (Array.isArray(data) && data.length === 0)) {
         console.log("here");
@@ -46,6 +93,7 @@ function DataDisplay() {
             <p>Table End</p>
         </div>
     );
+
     const imageBodyTemplate = (rowData) => {
         const imageUrl = rowData.itemImageURl;
         return <img src={imageUrl} alt={rowData.name} style={{ width: '100px', height: '100px' }} />;
@@ -60,7 +108,10 @@ function DataDisplay() {
 
     return (
 
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+            <div style={{ marginBottom: '10px' }}>
+                {currencyDropdown}
+            </div>
             {data && data.length > 0 ? (
                 <DataTable value={data}
                     header={header}
@@ -77,6 +128,8 @@ function DataDisplay() {
                     <Column field="store" header="Website" sortable />
                     <Column field="price" header="Price" sortable />
                     <Column header="Link" body={urlBodyTemplate} />
+                    {/* <Column field="convertedPrice" header={`Price (${selectedCurrency})`} */}
+                    <Column header={`Price (${selectedCurrency})`} body={priceBodyTemplate} sortable />
                 </DataTable>
             ) : (
                 <div>Sorry, couldn't find that item to compare.</div>
