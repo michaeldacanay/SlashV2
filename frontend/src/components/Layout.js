a6c4cf9fb5549601db63c33dca9766ca8df4927a
xliang8 <xliang8@ncsu.edu>
2023-11-26 14:51:53 -0500

    add backgorun color change

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Dropdown } from 'primereact/dropdown/dropdown.esm.js';
 import Navbar from './Navbar.js';
+import './custom.css';
 
 const Layout = ({ children, isAuthenticated }) => {
+
+    const [backgroundColor, setBackgroundColor] = useState('#ffffff'); // Default background color is white
+    const backgroundOptions = [
+        { label: 'White', value: '#ffffff' },
+        { label: 'Blue', value: '#3498db' },
+        { label: 'Green', value: '#2ecc71' },
+        { label: 'Yellow', value: '#f1c40f' },
+        { label: 'Red', value: '#e74c3c' },
+        // Add more color options as needed
+    ];
+    const onBackgroundColorChange = (e) => {
+        setBackgroundColor(e.value);
+        console.log('Background Color:', e.value);
+    };
     return (
-        <div>
-            <Navbar isAuthenticated={isAuthenticated} />
+        <div style={{ backgroundColor }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Navbar isAuthenticated={isAuthenticated} />
+                <div className="p-inputgroup" style={{ maxWidth: '150px' }}>
+                    <span className="p-inputgroup-addon">
+                        <i className="pi pi-palette" />
+                    </span>
+                    <Dropdown
+                        value={backgroundColor}
+                        options={backgroundOptions}
+                        onChange={onBackgroundColorChange}
+                        optionLabel="label"
+                        placeholder="Select Background Color"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
             <div>{children}</div>
         </div>
     );
